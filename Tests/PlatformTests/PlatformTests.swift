6605c101d246ee520708909f37624d1949f24785
Tony Freeman <tonyfreeman@users.noreply.github.com>
2017-01-24 12:20:11 +0000

    Implement arc4random for Linux

--- Tests/PlatformTests/PlatformTests.swift
+++ Tests/PlatformTests/PlatformTests.swift
@@ -12,13 +12,31 @@ import XCTest
 @testable import Platform
 
 class PlatformTests: XCTestCase {
-    func testExample() {
-
+    func testRandom() {
+        let first = arc4random()
+        let second = arc4random()
+        XCTAssertNotEqual(first, second)
     }
+
+    func testRandomUniform() {
+        for _ in 0..<1_000 {
+            XCTAssert(arc4random_uniform(12) < 12)
+        }
+    }
+
+    func testRandomBuffer() {
+        var first = [UInt8](repeating: 0, count: 10)
+        var second = [UInt8](repeating: 0, count: 10)
+        arc4random_buf(&first, first.count)
+        arc4random_buf(&second, second.count)
+        XCTAssertNotEqual(first, second)
+    }
+
 
     static var allTests : [(String, (PlatformTests) -> () throws -> Void)] {
         return [
-            ("testExample", testExample),
+            ("testRandom", testRandom),
+            ("testRandomUniform", testRandomUniform),
         ]
     }
 }
