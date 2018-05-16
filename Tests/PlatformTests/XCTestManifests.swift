3d5285db3b91571b21048217a7412c35567da31e
Tony Freeman <tonyfreeman@users.noreply.github.com>
2018-05-16 11:38:12 +0000

    Generate LinuxMain.swift

--- Tests/PlatformTests/XCTestManifests.swift
+++ Tests/PlatformTests/XCTestManifests.swift
@@ -6,19 +6,10 @@ extension EnvironmentTests {
     ]
 }
 
-extension PlatformTests {
-    static let __allTests = [
-        ("testRandom", testRandom),
-        ("testRandomBuffer", testRandomBuffer),
-        ("testRandomUniform", testRandomUniform),
-    ]
-}
-
 #if !os(macOS)
 public func __allTests() -> [XCTestCaseEntry] {
     return [
         testCase(EnvironmentTests.__allTests),
-        testCase(PlatformTests.__allTests),
     ]
 }
 #endif
